a638289f924913472f5211ebbf432ee967a53158
Lara <laraellenh@gmail.com>
2022-08-08 14:18:22 -0400

    CORS

--- Downloads/facebook/backend/server.js
+++ Downloads/facebook/backend/server.js
@@ -2,12 +2,31 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const options = {
-  origin: "http://localhost:3000",
-  useSuccessStatus: 200,
-}
+// practice with securing  CORS
+// const options = {
+//   origin: "http://localhost:3000",
+//   useSuccessStatus: 200,
+// }
+// let allowed = ["http://localhost:3000"];
+// function options(req, res){
+//   let tmp;
+//   let origin= req.header("Origin");
+//   if(allowed.indexOf(origin)> -1){
+//     tmp={
+//       origin: true,
+//       optionSuccessStatus: 200,
+//     };
+//   } else {
+//     tmp={
+//       origin: "stupid",
+//     };
+//   }
+//   res(null, tmp);
+// }
+// app.use(cors(options));
 // create the endpoints
-app.use(cors(options));
+// less secure CORS for this project
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("welcome from home");
 });
