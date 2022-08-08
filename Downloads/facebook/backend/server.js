e7ba161785aff607d195c9462a12efd2b16a0d6c
Lara <laraellenh@gmail.com>
2022-08-08 11:27:48 -0400

    updates to server

--- Downloads/facebook/backend/server.js
+++ Downloads/facebook/backend/server.js
@@ -1,7 +1,13 @@
 // import express and make it  a function
-const express =require("express");
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const options = {
+  origin: "http://localhost:3000",
+  useSuccessStatus: 200,
+}
 // create the endpoints
+app.use(cors(options));
 app.get("/", (req, res) => {
   res.send("welcome from home");
 });
