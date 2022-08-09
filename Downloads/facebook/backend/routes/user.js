559929070a44bb68bd6f5dc14d67845a8514246a
Lara <laraellenh@gmail.com>
2022-08-09 15:37:30 -0400

    routes directory added, home function created to improve routing

--- Downloads/facebook/backend/routes/user.js
+++ Downloads/facebook/backend/routes/user.js
@@ -1,5 +1,9 @@
 
 const express = require("express");
+
+const { home } = require("../controllers/users");
+
 const router = express.Router();
+
 router.get('/user', home);
 module.exports = router;
