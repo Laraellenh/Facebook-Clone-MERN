dfc508fb79f2a5d4b1c3de5715c42bf3db0890a9
Lara <laraellenh@gmail.com>
2022-08-09 15:19:31 -0400

    added controllers directory and users controller, updated the fetch route for /user

--- Downloads/facebook/backend/routes/user.js
+++ Downloads/facebook/backend/routes/user.js
@@ -1,7 +1,5 @@
 
 const express = require("express");
 const router = express.Router();
-router.get('/user', (req, res) => {
-    res.send('welcome from user home')
-});
+router.get('/user', home);
 module.exports = router;
