8693aef7f217e3280b91963b0248c80d0b5a0729
Lara <laraellenh@gmail.com>
2022-08-10 15:43:02 -0400

    updates to environment variable (Port)

--- Downloads/facebook/backend/server.js
+++ Downloads/facebook/backend/server.js
@@ -3,11 +3,16 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const {readdirSync} = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
 app.use(cors());
+readdirSync("./routes").map((r) => app.use("/", require("./routes/"+r)));
+const PORT = process.env.PORT || 8000;
 // synchronously read the contents of a directory in node The method returns an array with all the file names or objects in the directory. The options argument can be used to change the format in which the files are returned from the method.
 // Syntax:fs.readdirSync( path, options )
 // dynamically add routes by mapping the array our routes!
-readdirSync("./routes").map((r) => app.use("/", require("./routes/"+r)));
-app.listen(8000, () => {
-  console.log("server is listening...");
+
+
+app.listen(PORT, () => {
+  console.log(`server is on port ${PORT}`);
 });
