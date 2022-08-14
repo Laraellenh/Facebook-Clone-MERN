f539ce9d567e4a538db6b51454c19dae069cf8bc
Lara <laraellenh@gmail.com>
2022-08-14 16:11:44 -0400

    got mongodb successfully connected, .env folder needed to be moved  into the backend directory

--- Downloads/facebook/backend/server.js
+++ Downloads/facebook/backend/server.js
@@ -1,12 +1,13 @@
 // import express and make it  a function
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const app = express();
 const {readdirSync} = require("fs");
 const dotenv = require("dotenv");
 dotenv.config();
 app.use(cors());
+// routes
 readdirSync("./routes").map((r) => app.use("/", require("./routes/"+r)));
 const PORT = process.env.PORT || 8000;
 // synchronously read the contents of a directory in node The method returns an array with all the file names or objects in the directory. The options argument can be used to change the format in which the files are returned from the method.
@@ -15,11 +16,12 @@ const PORT = process.env.PORT || 8000;
 
 //db
 mongoose
-.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-})
-.then(() => console.log("db connected"))
-.catch((err) => console.log("error connecting to mongodb", err));
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("database connected successfully"))
+  .catch((err) => console.log("error connecting to mongodb", err));
+
 app.listen(PORT, () => {
   console.log(`server is on port ${PORT}`);
 });
