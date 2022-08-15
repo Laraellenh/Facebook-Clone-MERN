30c349e73bba7bbfb425fb8f30751902f6f8cd59
Lara <laraellenh@gmail.com>
2022-08-15 16:49:40 -0400

    set up the register post request, for some reason its persisting locally but not to mongo atlas, going to look into this.

--- Downloads/facebook/backend/controllers/user.js
+++ Downloads/facebook/backend/controllers/user.js
@@ -1,3 +1,31 @@
-exports.register = (req, res) => {
-    console.log(req.body);
+const User = require("../models/User");
+exports.register = async (req, res) => {
+   try { 
+    const {
+        first_name,
+        last_name,
+        birth_year,
+        birth_month,
+        birth_day,
+        gender,
+        username,
+        email,
+        password
+        } = req.body;
+    // could use syntax conser user = await new User ({req.body})in place of specifics
+    const user = await new User({ 
+        first_name,
+        last_name,
+        birth_year,
+        birth_month,
+        birth_day,
+        gender,
+        username,
+        email,
+        password,
+    }).save();
+    res.json(user); 
+    } catch (error) {
+     res.status(500).json({ message: error.message});
+    }
 };
